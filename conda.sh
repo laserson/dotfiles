8e6e5615c6b7f3e0a70f442f9f4034a41c7178de
Uri Laserson <uri.laserson@gmail.com>
2017-11-12 14:06:04 -0500

    More conda/python packages

--- conda.sh
+++ conda.sh
@@ -1,10 +1,12 @@
-PACKAGES="cython numpy scipy pandas scikit-learn nltk jupyter matplotlib"
+PACKAGES1="cython numpy scipy pandas scikit-learn jupyter matplotlib biopython scikit-bio spacy"
+PACKAGES2="awscli beautifulsoup4 bokeh boto3 click cytoolz DendroPy dill Fabric flake8 googlemaps ipdb jellyfish Jinja2 jinja2-cli luigi pelican seaborn Sphinx thrift google-api-python-client"
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda-install.sh
 bash miniconda-install.sh -b
 export PATH="~/miniconda3/bin:$PATH"
 conda update -y conda
-conda install -y $PACKAGES
-conda create -y -n py27 python=2.7 $PACKAGES
-conda create -y -n py35 python=3.5 $PACKAGES
+conda install -y $PACKAGES1
+conda create -y -n py27 python=2.7 $PACKAGES1
+conda create -y -n py36 python=3.6 $PACKAGES1
 conda info -a
 rm miniconda-install.sh
+pip install $PACKAGES2
